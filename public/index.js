1741a5c02f2121f16bccc3a06af826f33d888849
Phrosen63 <peter.persson1@gmail.com>
2021-02-06 15:34:23 +0100

    Setup simple connection to firestore.

--- public/index.js
+++ public/index.js
@@ -1,3 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
   const app = firebase.app();
+  const body = document.body;
+
+  const db = firebase.firestore();
+
+  db.collection('dod-online').get().then(snapshot => {
+    snapshot.docs.forEach(doc => {
+      let data = doc.data();
+      console.log(data);
+
+      let adress = document.createElement('p');
+      let age = document.createElement('p');
+      let name = document.createElement('p');
+
+      adress.textContent = data.adress;
+      age.textContent = data.age;
+      name.textContent = data.name;
+
+      body.appendChild(adress);
+      body.appendChild(age);
+      body.appendChild(name);
+    });
+  });
+  
 });
