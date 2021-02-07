d94f6f515742f3de717f1e7682d82d8b8adc4c11
Phrosen63 <peter.persson1@gmail.com>
2021-02-07 15:42:35 +0100

    Add re-direct if user is not logged in.

--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -5,7 +5,22 @@
 </template>
 
 <script>
+import { auth } from '@/api/auth';
+
 export default {
   name: 'Lobby',
+  created() {
+    auth.onAuthStateChanged(firebaseUser => {
+      if (firebaseUser) {
+        // User is logged in
+        this.$store.commit('setUser', firebaseUser);
+        const btnLogout = document.getElementById('btnLogout');
+        btnLogout.classList.remove('hide');
+      } else {
+        // User is not logged in
+        this.$router.push('/');
+      }
+    });
+  },
 }
 </script>
