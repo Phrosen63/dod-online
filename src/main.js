c3b238682b8f26c5fc4ac881dd2bfa19b9b18de5
Phrosen63 <peter.persson1@gmail.com>
2021-02-07 14:38:43 +0100

    Add Vuex.

--- src/main.js
+++ src/main.js
@@ -2,12 +2,17 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import { firestorePlugin } from 'vuefire';
+import Vuex from 'vuex';
+
+import store from './store/index';
 
 Vue.use(firestorePlugin);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(Vuex);
 
 new Vue({
   router,
+  store,
   render: function (h) { return h(App) }
 }).$mount('#app');
