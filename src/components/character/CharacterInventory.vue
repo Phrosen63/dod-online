f4a72260175e156a5bddac814fccb99184480282
Phrosen63 <peter.persson1@gmail.com>
2021-02-09 20:42:59 +0100

    Style CharacterInventory.

--- src/components/character/CharacterInventory.vue
+++ src/components/character/CharacterInventory.vue
@@ -2,13 +2,23 @@
   <div class="character-inventory">
     <h2 v-if="Object.keys(inventory).length > 0">Inventory</h2>
     <ul>
-      <li v-for="(item, key) in inventory" :key="key">
-        <p>
+      <li v-for="(item, key) in inventory" :key="key" class="character-inventory-item">
+        <p class="character-inventory-item__name">
           {{ key }}
         </p>
-        <p>
-          {{ item }}
-        </p>
+        <ul>
+          <li 
+            v-for="(attribute, key) in item" :key="key" 
+            class="character-inventory-attribute-list"
+          >
+            <span>
+              {{ key }}:
+            </span>
+            <span>
+              {{ attribute }}
+            </span>
+          </li>
+        </ul>
       </li>
     </ul>
   </div>
@@ -30,5 +40,14 @@ export default {
 <style scoped>
 .character-inventory {
   flex: 1 0 auto;
+}
+
+.character-inventory-item__name {
+  font-size: 20px;
+  margin: 20px 0 10px 0;
+}
+
+.character-inventory-attribute-list {
+  margin: 0 0 0 10px;
 }
 </style>
