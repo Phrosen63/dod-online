4d45af6846534ffc9cab2c594a2591e11edfde12
Phrosen63 <peter.persson1@gmail.com>
2021-02-09 20:21:03 +0100

    Add CharacterInfo.

---
+++ src/components/character/CharacterInfo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="character-info">
+    <h2>Info</h2>
+    <div>
+      <span>
+        Name:
+      </span>
+      <span>
+        {{ info.name }}
+      </span>
+    </div>
+    <div>
+      <span>
+        Class:
+      </span>
+      <span>
+        {{ info.class }}
+      </span>
+    </div>
+    <div>
+      <span>
+        Race:
+      </span>
+      <span>
+        {{ info.race }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CharacterInfo',
+  props: {
+    info: {
+      default: {},
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.character-info span {
+  font-size: 18px;
+}
+</style>
