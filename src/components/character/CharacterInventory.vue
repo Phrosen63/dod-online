111e61da227fcace284abc30cfb0fa2548dc34b2
Phrosen63 <peter.persson1@gmail.com>
2021-02-09 20:21:09 +0100

    Add CharacterInventory.

---
+++ src/components/character/CharacterInventory.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="character-inventory">
+    <h2 v-if="Object.keys(inventory).length > 0">Inventory</h2>
+    <ul>
+      <li v-for="(item, key) in inventory" :key="key">
+        <p>
+          {{ key }}
+        </p>
+        <p>
+          {{ item }}
+        </p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CharacterInventory',
+  props: {
+    inventory: {
+      default: {},
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.character-inventory {
+  flex: 1 0 auto;
+}
+</style>
