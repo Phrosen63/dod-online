63b901f63517c23a7a0bbdd61058c3065479932a
Phrosen63 <peter.persson1@gmail.com>
2021-02-10 21:29:09 +0100

    Update eslint settings.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,4 +6,12 @@ module.exports = {
   rules: {
     "prefer-default-export": 0,
   },
+  parserOptions: {
+    ecmaVersion: 8,
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
 };
