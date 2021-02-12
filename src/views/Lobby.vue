e46874df374112578994c5096e17280afb28c7a6
Phrosen63 <peter.persson1@gmail.com>
2021-02-12 20:28:32 +0100

    Update view Lobby.

--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -1,11 +1,43 @@
 <template>
-  <div>
-    <p>This is the Lobby page!</p>
+  <div class="lobby">
+    <h1>Lobby</h1>
+    <div class="lobby-wrapper">
+      <Dice />
+      <Console />
+    </div>
   </div>
 </template>
 
 <script>
+// Components
+import Dice from '@/components/lobby/Dice';
+import Console from '@/components/lobby/Console';
+
 export default {
   name: 'Lobby',
+  components: {
+    Dice,
+    Console,
+  },
 };
 </script>
+
+<style scoped>
+.lobby {
+  width: 80%;
+  margin: 100px auto;
+  background-color: white;
+  border-radius: 5px;
+  padding: 20px;
+  min-height: 500px;
+}
+
+.lobby-wrapper {
+  display: flex;
+  flex-direction: row;
+}
+
+h1 {
+  text-align: center;
+}
+</style>
