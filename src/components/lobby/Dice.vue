4039d7d076b4b5d8721cafe8f7b940857df287f1
Phrosen63 <peter.persson1@gmail.com>
2021-02-12 20:28:15 +0100

    Add component Dice.

---
+++ src/components/lobby/Dice.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="dice">
+    <button @click="rollDie">
+      Roll die!
+    </button>
+  </div>
+</template>
+
+<script>
+// Modules
+import { writeObject } from '@/api/writeToDb';
+import { getFirebaseUser } from '@/api/getUserData';
+
+export default {
+  name: 'Dice',
+  data() {
+    return {
+      userDisplayName: '',
+    };
+  },
+  created() {
+    this.getUserDisplayName();
+  },
+  methods: {
+    async getUserDisplayName() {
+      // TODO: Create method for this in API
+      const currentUser = await getFirebaseUser();
+      this.userDisplayName = currentUser.displayName;
+    },
+    rollDie() {
+      // TODO: Create API for random numbers
+      const max = 6;
+      const min = 1;
+      let result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+      const data = {
+        user: this.userDisplayName,
+        value: result,
+        dice: 'T6',
+      }
+      writeObject('console', 'shared', data);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.dice {
+  flex: 0 0 20%;
+}
+</style>
