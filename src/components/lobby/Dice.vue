df4ad1b9f752af02e77400e140a6e9cbad051a2b
Phrosen63 <peter.persson1@gmail.com>
2021-02-13 11:38:07 +0100

    Use rollDie in component Dice, Use getUserDisplayName in component Dice.

--- src/components/lobby/Dice.vue
+++ src/components/lobby/Dice.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="dice">
-    <button @click="rollDie">
-      Roll die!
+    <button @click="rollDie('T4')">
+      T4
+    </button>
+    <button @click="rollDie('T6')">
+      T6
+    </button>
+    <button @click="rollDie('T8')">
+      T8
+    </button>
+    <button @click="rollDie('T12')">
+      T12
+    </button>
+    <button @click="rollDie('T20')">
+      T20
+    </button>
+    <button @click="rollDie('T100')">
+      T100
     </button>
   </div>
 </template>
@@ -9,34 +24,27 @@
 <script>
 // Modules
 import { writeObject } from '@/api/database/writeToDb';
-import { getFirebaseUser } from '@/api/database/getUserData';
+import { getUserDisplayName } from '@/api/database/getUserData';
+import { rollDie } from '@/api/randomNumberGenerator';
 
 export default {
   name: 'Dice',
   data() {
     return {
-      userDisplayName: '',
+      userDisplayName: undefined,
     };
   },
   created() {
-    this.getUserDisplayName();
+    this.userDisplayName = this.setUserDisplayName();
   },
   methods: {
-    async getUserDisplayName() {
-      // TODO: Create method for this in API
-      const currentUser = await getFirebaseUser();
-      this.userDisplayName = currentUser.displayName;
+    async setUserDisplayName() {
+      this.userDisplayName = await getUserDisplayName();
     },
-    rollDie() {
-      // TODO: Create API for random numbers
-      const max = 6;
-      const min = 1;
-      let result = Math.floor(Math.random() * (max - min + 1)) + min;
-
+    rollDie(die) {
+      const result = rollDie(die);
       const data = {
-        user: this.userDisplayName,
-        value: result,
-        dice: 'T6',
+        message: `${this.userDisplayName} rolled: ${result.value}, with a: ${result.die}`,
       }
       writeObject('console', 'shared', data);
     },
@@ -47,5 +55,20 @@ export default {
 <style scoped>
 .dice {
   flex: 0 0 20%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 0 10px;
+}
+
+.dice > button {
+  flex: 0 0 auto;
+  margin: 0 10%;
+  background-color: tomato;
+  border-radius: 10%;
+  border: 1px solid;
+  width: 60px;
+  height: 60px;
+  cursor: pointer;
 }
 </style>
