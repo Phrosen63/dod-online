55b6993befdc2d4cf4431dc143779e86b0345865
Phrosen63 <peter.persson1@gmail.com>
2021-02-16 18:45:07 +0100

    Add Roll custom die button.

--- src/components/lobby/Dice.vue
+++ src/components/lobby/Dice.vue
@@ -8,17 +8,40 @@
     <Die :data="{ title: 'Roll T12', die: 'T12' }" />
     <Die :data="{ title: 'Roll 204', die: 'T20' }" />
     <Die :data="{ title: 'Roll100T4', die: 'T100' }" />
+    <button
+      class="die custom-die"
+      @click="showModal"
+    >
+      Roll custom die
+    </button>
   </div>
 </template>
 
 <script>
 // Components
 import Die from './Die';
+import CustomDice from '../modals/CustomDice'
 
 export default {
   name: 'Dice',
   components: {
     Die,
+  },
+  methods: {
+    showModal() {
+      const componentProps = {};
+      const modalProps = {
+        height: 'auto',
+        draggable: true,
+        focusTrap: true,
+      };
+
+      this.$modal.show(
+        CustomDice,
+        componentProps,
+        modalProps,
+      );
+    },
   },
 };
 </script>
@@ -29,7 +52,8 @@ export default {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  height: 270px;
+  height: 360px;
+  margin: 41px 0 0 0;
   padding: 0 10px;
 }
 
@@ -44,5 +68,9 @@ export default {
   color: #fff;
   font-size: 22px;
   cursor: pointer;
+}
+
+.custom-die {
+  flex: 1 0 auto;
 }
 </style>
