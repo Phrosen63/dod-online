f92174b738a376ed656907bffad8a781632ca807
Phrosen63 <peter.persson1@gmail.com>
2021-02-16 18:44:00 +0100

    Add customDiceRoll state and mutator.

--- src/store/index.js
+++ src/store/index.js
@@ -6,10 +6,16 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     user: null,
+    customDiceRoll: null,
   },
   mutations: {
     setUser(state, payload) {
       state.user = payload;
+    },
+    setCustomDiceRoll(state, payload) {
+      if (payload && typeof payload === 'object' && payload.message) {
+        state.customDiceRoll = payload;
+      }
     },
   },
 });
