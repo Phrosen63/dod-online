607df1b2254795b8c2bb3f46d017e0754c22a34b
Phrosen63 <peter.persson1@gmail.com>
2021-02-21 18:06:28 +0100

    Make CharacterInventory items edit- and deleteable.

--- src/components/character/CharacterInventory.vue
+++ src/components/character/CharacterInventory.vue
@@ -1,35 +1,49 @@
 <template>
   <div class="character-inventory">
     <h2>Inventory</h2>
-    <ul>
+    <transition-group
+      name="transition-list"
+      tag="ul"
+    >
       <li
-        v-for="(item, key) in inventory"
-        :key="key"
+        v-for="item in inventory"
+        :key="item.id"
         class="character-inventory-item"
       >
-        <p class="character-inventory-item__name">
-          {{ key }}
+        <p
+          v-for="(value, key, index) in item"
+          :key="index"
+          :data-type="key.toLowerCase()"
+        >
+          {{ key }}:
+          {{ value }}
         </p>
-        <ul>
-          <li
-            v-for="(value, attribute) in item"
-            :key="attribute"
-            class="character-inventory-attribute-list"
+        <div class="item-controls">
+          <button
+            class="item-control item-control__edit"
+            :data-id="item.id"
+            @click="clickEdit(item.id)"
           >
-            <span>
-              {{ attribute }}:
-            </span>
-            <span>
-              {{ value }}
-            </span>
-          </li>
-        </ul>
+            Edit
+          </button>
+          <button
+            class="item-control item-control__delete"
+            :data-id="item.id"
+            @click="clickDelete(item.id)"
+          >
+            Delete
+          </button>
+        </div>
       </li>
-    </ul>
+    </transition-group>
   </div>
 </template>
 
 <script>
+// Componentes
+import AddOrEditFieldModal from '@/components/modals/AddOrEditFieldModal';
+import { deleteDocumentFromCurrentUser } from '@/api/database/delete';
+
 export default {
   name: 'CharacterInventory',
   props: {
@@ -41,13 +55,97 @@ export default {
       },
     },
     inventory: {
-      type: Object,
+      type: Array,
       required: true,
       default() {
-        return {};
+        return [];
       },
     },
   },
+  data() {
+    return {
+      INVENTORY_COLLECTION: undefined,
+    };
+  },
+  computed: {
+    characterInventorySavedListener() {
+      return this.$store.state.inventoryItemSaved;
+    },
+  },
+  watch: {
+    characterInventorySavedListener(arr) {
+      arr.forEach(obj => {
+        const target = this.inventory.find((item) => item.id === obj.id);
+
+        if (target) {
+          // Update existing object
+          target[obj.key] = obj.value;
+        } else {
+          // Add new object
+          if (obj.id) {
+            this.inventory.push(obj);
+          }
+        }
+      });
+    }
+  },
+  created() {
+    this.INVENTORY_COLLECTION = `characters/${this.characterId}/inventory`;
+  },
+  methods: {
+    getItemById(id) {
+      return this.inventory.find((item) => item.id === id);
+    },
+    clickEdit(itemId) {
+      const item = this.getItemById(itemId);
+      const data = [];
+
+      Object.keys(item).forEach(key => {
+        if (key !== 'id') {
+          const temp = {
+            id: itemId,
+            key: key,
+            value: item[key],
+            fieldName: key,
+          };
+          data.push(temp);
+        }
+      });
+
+      const componentProps = {
+        data,
+        title: {
+          key: 'Edit field',
+          value: item.key,
+        },
+        objectId: itemId,
+        characterId: this.characterId,
+        path: this.INVENTORY_COLLECTION,
+        mutation: 'setInventoryItemSaved',
+      };
+      const modalProps = {
+        height: 'auto',
+        scrollable: true,
+        focusTrap: true,
+      };
+
+      this.$modal.show(
+        AddOrEditFieldModal,
+        componentProps,
+        modalProps,
+      );
+    },
+    clickDelete(itemId) {
+      console.log(itemId)
+      const index = this.inventory.findIndex(item => item.id === itemId);
+      console.log(index)
+      if (index > -1) {
+        const item = this.inventory[index];
+        this.inventory.splice(index, 1);
+        deleteDocumentFromCurrentUser(this.INVENTORY_COLLECTION, item.id);
+      }
+    },
+  }
 };
 </script>
 
@@ -56,12 +154,53 @@ export default {
   flex: 1 0 auto;
 }
 
-.character-inventory-item__name {
-  margin: 20px 0 10px 0;
-  font-size: 20px;
+.transition-list-enter-active,
+.transition-list-leave-active {
+  transition: all 0.5s;
 }
 
-.character-inventory-attribute-list {
-  margin: 0 0 0 10px;
+.transition-list-enter,
+.transition-list-leave-to {
+  opacity: 0;
+}
+
+.character-inventory-item {
+  position: relative;
+}
+
+[data-type="id"] {
+  display: none;
+}
+
+.character-inventory-item--strikethrough p,
+.character-inventory-item--strikethrough span {
+  text-decoration: line-through;
+}
+
+.item-controls {
+  display: none;
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+
+.item-control {
+  cursor: pointer;
+}
+
+.item-control__edit {
+  margin: 0 5px 0 0;
+}
+
+.item-control__strike {
+  margin: 0 5px 0 0;
+}
+
+.character-inventory-item:hover {
+  background-color: #e4e4e4;
+}
+
+.character-inventory-item:hover > .item-controls {
+  display: block;
 }
 </style>
