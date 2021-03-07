fb97821b16ed5da882709460053aaa41265edb75
Phrosen63 <peter.persson1@gmail.com>
2021-03-07 12:38:29 +0100

    Add i18n to the project.

---
+++ src/i18n/i18n.js
@@ -0,0 +1,27 @@
+import VueI18n from 'vue-i18n';
+import Vue from 'vue';
+
+// Import language files
+// import en from './lang/en.json';
+// import sv from './lang/sv.json';
+
+Vue.use(VueI18n);
+
+function loadLocaleMessages() {
+  const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i);
+  const messages = {};
+  locales.keys().forEach(key => {
+    const matched = key.match(/([A-Za-z0-9-_]+)\./i)
+    if (matched && matched.length > 1) {
+      const locale = matched[1];
+      messages[locale] = locales(key);
+    }
+  });
+  return messages;
+}
+
+export default new VueI18n({
+  locale: 'en',
+  fallbackLocale: 'sv',
+  messages: loadLocaleMessages(),
+});
--- src/main.js
+++ src/main.js
@@ -1,23 +1,35 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { firestorePlugin } from 'vuefire';
 import App from './App.vue';
+
+// Firestore
+import { firestorePlugin } from 'vuefire';
+
+// Vue router
 import router from './router';
+
+// Modal
 import VModal from 'vue-js-modal';
 
-import VueTabs from 'vue-nav-tabs'
-import 'vue-nav-tabs/themes/vue-tabs.css'
+// Tabs
+import VueTabs from 'vue-nav-tabs';
+import 'vue-nav-tabs/themes/vue-tabs.css';
 
+// Store
 import store from './store/index';
+
+// Translations
+import i18n from './i18n/i18n';
 
 Vue.config.productionTip = false;
 Vue.use(firestorePlugin);
 Vue.use(Vuex);
 Vue.use(VModal);
-Vue.use(VueTabs)
+Vue.use(VueTabs);
 
 new Vue({
   router,
   store,
+  i18n,
   render(h) { return h(App); },
 }).$mount('#app');
