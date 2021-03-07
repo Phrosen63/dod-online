a23bdab9aeeae54579b70c80f6b1f4973086c3b6
Phrosen63 <peter.persson1@gmail.com>
2021-03-07 23:14:10 +0100

    Add field for prefered language in view Profile.

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -14,7 +14,7 @@
       <div>
         <label
           for="field_UserName"
-          class="profile-info__user-name"
+          class="profile-info__label profile-info__user-name"
         >
           {{ $t('user_name') }}:
         </label>
@@ -36,6 +36,26 @@
       </div>
       <p>{{ $t('email') }}: <span>{{ user.email }}</span></p>
       <p>{{ $t('role') }}: <span>{{ role }}</span></p>
+      <div>
+        <label
+          for="language"
+          class="profile-info__label"
+        >
+          {{ $t('prefered_language') }}
+        </label>
+        <select
+          id="language"
+          v-model="language"
+          @change="languageUpdated($event)"
+        >
+          <option value="en">
+            en
+          </option>
+          <option value="sv">
+            sv
+          </option>
+        </select>
+      </div>
     </div>
   </div>
 </template>
@@ -46,6 +66,7 @@ import { db } from '@/api/database/db';
 import { getFirebaseUser } from '@/api/database/user';
 import { PulseLoader } from 'vue-spinner/dist/vue-spinner.min';
 import { generateName } from '@/api/randomNameGenerator';
+import { updateDocumentFieldForCurrentUser } from '@/api/database/write';
 
 export default {
   name: 'Profile',
@@ -56,6 +77,8 @@ export default {
     return {
       user: {},
       displayName: undefined,
+      settingsId: undefined,
+      language: undefined,
       role: undefined,
       loading: true,
       color: '#75a1de',
@@ -85,10 +108,16 @@ export default {
       const currentUser = this.user ? this.user : await getFirebaseUser();
       const { uid } = currentUser;
       const COLLECTION_NAME = `/users/${uid}/settings`;
+
       const snapshot = await db.collection(COLLECTION_NAME).get();
-      const result = snapshot.docs.map((doc) => doc.data());
-      const role = result[0].role;
-      return this.convertRole(role);
+      snapshot.docs.map((doc) => {
+        this.settingsId = doc.id || undefined;
+        const data = doc.data();
+        this.role = data.role || 2;
+        this.language = data.language || 'en';
+      });
+
+      return this.convertRole(this.role);
     },
     updateUserDisplayName(event) {
       if (this.user) {
@@ -106,12 +135,23 @@ export default {
       this.updateUserDisplayName(fakeEvent);
       this.displayName = randomName;
     },
+    async languageUpdated(event) {
+      const data = {
+        language: event.target.value,
+      };
+
+      updateDocumentFieldForCurrentUser({
+        collection: 'settings',
+        document: this.settingsId,
+        data,
+      }).catch(e => console.log('Error: ' + e));
+    },
   },
 };
 </script>
 
 <style scoped>
-.profile-info__user-name {
+.profile-info__label {
   font-size: 18px;
 }
 
