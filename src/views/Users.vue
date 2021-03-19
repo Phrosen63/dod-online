2156e3eaa3dab5a0ea7054b9984ff6b4f355759a
Phrosen63 <peter.persson1@gmail.com>
2021-03-19 18:39:39 +0100

    List selected character info in view Users, List selected character stats in view Users.

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -10,18 +10,69 @@
       <h1>{{ $t('users') }}</h1>
       <div class="grid">
         <div class="row">
-          <div class="col-4">
-            <ul>
+          <div class="col-3">
+            <h2>{{ $t('users') }}</h2>
+            <ul class="user-list">
               <li
                 v-for="user in list"
                 :key="user.uid"
+                class="user-list-item"
+                :class="isSelected(user.uid)"
+                @click="selectUser(user.uid)"
               >
                 {{ user.displayName }}
               </li>
             </ul>
           </div>
-          <div class="col-8">
-            col-8
+          <div class="col-9">
+            <pulse-loader
+              v-if="userLoading"
+              class="spinner"
+              :size="size"
+              :color="color"
+            />
+            <div v-else>
+              <h2>{{ $t('characters') }}</h2>
+              <ul
+                v-if="selectedUser"
+                class="character-list"
+              >
+                <li
+                  v-for="character in selectedUser.characters"
+                  :key="getCharacterKey(character.info.name)"
+                  class="character-list-item"
+                >
+                  <div class="grid">
+                    <div class="row">
+                      <div class="col-4">
+                        <p>
+                          {{ $t('name') }}: {{ character.info.name }}
+                        </p>
+                        <p>
+                          {{ $t('race') }}: {{ character.info.race }}
+                        </p>
+                        <p>
+                          {{ $t('class') }}: {{ character.info.class }}
+                        </p>
+                      </div>
+                      <div class="col-8">
+                        <ul class="stats-list">
+                          <li
+                            v-for="(stat, key) in character.stats"
+                            :key="key"
+                            class="stats-list-item"
+                          >
+                            <p class="stat">
+                              {{ $t(key.toLowerCase()) }}: {{ stat }}
+                            </p>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
@@ -42,9 +93,11 @@ export default {
   data() {
     return {
       loading: true,
+      userLoading: false,
       color: '#75a1de',
       size: '25px',
       list: [],
+      selectedUser: undefined,
     };
   },
   async created() {
@@ -70,10 +123,59 @@ export default {
         displayName,
         characters,
         settingId,
+        selected: false,
       })
     });
 
     this.loading = false;
   },
+  methods: {
+    selectUser(uid) {
+      this.userLoading = true;
+      this.selectedUser = this.list.find(user => user.uid === uid);
+
+      this.list.forEach(user => user.selected = false);
+      this.selectedUser.selected = true;
+
+      this.userLoading = false;
+    },
+    isSelected(uid) {
+      return this.selectedUser && this.selectedUser.uid === uid ? 'user-list-item--selected' : '';
+    },
+    getCharacterKey(characterName) {
+      return `${this.selectedUser.uid}_${characterName}`;
+    },
+  },
 };
 </script>
+
+<style scoped>
+.user-list-item {
+  cursor: pointer;
+  margin: 10px 0;
+}
+
+.user-list-item--selected {
+  font-weight: bold;
+  background-color: #ffeac4;
+}
+
+.user-list-item:hover {
+  background-color: #ffeac4;
+}
+
+.character-list-item {
+  background-color: #b5cdef;
+  border-radius: 10px;
+  padding: 10px;
+  margin: 10px 0;
+}
+
+.character-list-item:first-of-type {
+  margin: 0 0 10px 0;
+}
+
+.stat {
+  margin: 5px 0;
+}
+</style>
