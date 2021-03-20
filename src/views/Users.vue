a589fdc272e86a6ed1c410f27016c034dbb7cbe4
Phrosen63 <peter.persson1@gmail.com>
2021-03-20 09:22:54 +0100

    Refactor view Users.

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -12,17 +12,10 @@
         <div class="row">
           <div class="col-3">
             <h2>{{ $t('users') }}</h2>
-            <ul class="user-list">
-              <li
-                v-for="user in list"
-                :key="user.uid"
-                class="user-list-item"
-                :class="isSelected(user.uid)"
-                @click="selectUser(user.uid)"
-              >
-                {{ user.displayName }}
-              </li>
-            </ul>
+            <UserList
+              :list="list"
+              @userSelected="selectUser($event)"
+            />
           </div>
           <div class="col-9">
             <pulse-loader
@@ -33,45 +26,11 @@
             />
             <div v-else>
               <h2>{{ $t('characters') }}</h2>
-              <ul
+              <UserCharactersList
                 v-if="selectedUser"
-                class="character-list"
-              >
-                <li
-                  v-for="character in selectedUser.characters"
-                  :key="getCharacterKey(character.info.name)"
-                  class="character-list-item"
-                >
-                  <div class="grid">
-                    <div class="row">
-                      <div class="col-4">
-                        <p>
-                          {{ $t('name') }}: {{ character.info.name }}
-                        </p>
-                        <p>
-                          {{ $t('race') }}: {{ character.info.race }}
-                        </p>
-                        <p>
-                          {{ $t('class') }}: {{ character.info.class }}
-                        </p>
-                      </div>
-                      <div class="col-8">
-                        <ul class="stats-list">
-                          <li
-                            v-for="(stat, key) in character.stats"
-                            :key="key"
-                            class="stats-list-item"
-                          >
-                            <p class="stat">
-                              {{ $t(key.toLowerCase()) }}: {{ stat }}
-                            </p>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-              </ul>
+                :characters="selectedUser.characters"
+                :uid="selectedUser.uid"
+              />
             </div>
           </div>
         </div>
@@ -81,6 +40,10 @@
 </template>
 
 <script>
+// Components
+import UserList from '@/components/users/UserList';
+import UserCharactersList from '@/components/users/UserCharactersList';
+
 // Modules
 import { PulseLoader } from 'vue-spinner/dist/vue-spinner.min';
 import { db } from '@/api/database/db';
@@ -89,6 +52,8 @@ export default {
   name: 'Users',
   components: {
     PulseLoader,
+    UserList,
+    UserCharactersList,
   },
   data() {
     return {
@@ -105,11 +70,19 @@ export default {
     const snapshot = await db.collection(USERS_COLLECTION).get();
     const userIds = snapshot.docs.map((doc) => doc.id);
 
+    // Characters
     userIds.forEach(async uid => {
       const CHARACTERS_COLLECTION = `/users/${uid}/characters`;
       const characterSnapshot = await db.collection(CHARACTERS_COLLECTION).get();
       const characters = characterSnapshot.docs.map((doc) => doc.data());
 
+      for (let i = 0; i < characterSnapshot.docs.length; i += 1) {
+        // Character id
+        const characterId = characterSnapshot.docs[i].id;
+        characters[i].id = characterId;
+      }
+
+      // Settings
       const SETTINGS_COLLECTION = `/users/${uid}/settings`;
       const settingsSnapshot = await db.collection(SETTINGS_COLLECTION).get();
       const settingId = settingsSnapshot.docs.map((doc) => doc.id)[0];
@@ -130,7 +103,8 @@ export default {
     this.loading = false;
   },
   methods: {
-    selectUser(uid) {
+    selectUser(e) {
+      const uid = e;
       this.userLoading = true;
       this.selectedUser = this.list.find(user => user.uid === uid);
 
@@ -139,43 +113,6 @@ export default {
 
       this.userLoading = false;
     },
-    isSelected(uid) {
-      return this.selectedUser && this.selectedUser.uid === uid ? 'user-list-item--selected' : '';
-    },
-    getCharacterKey(characterName) {
-      return `${this.selectedUser.uid}_${characterName}`;
-    },
   },
 };
 </script>
-
-<style scoped>
-.user-list-item {
-  cursor: pointer;
-  margin: 10px 0;
-}
-
-.user-list-item--selected {
-  font-weight: bold;
-  background-color: #ffeac4;
-}
-
-.user-list-item:hover {
-  background-color: #ffeac4;
-}
-
-.character-list-item {
-  background-color: #b5cdef;
-  border-radius: 10px;
-  padding: 10px;
-  margin: 10px 0;
-}
-
-.character-list-item:first-of-type {
-  margin: 0 0 10px 0;
-}
-
-.stat {
-  margin: 5px 0;
-}
-</style>
