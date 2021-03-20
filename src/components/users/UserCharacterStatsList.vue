5f95e4ccfd12feef34b16af80fc5fc4ad9d59f2e
Phrosen63 <peter.persson1@gmail.com>
2021-03-20 09:22:45 +0100

    Add component UsersCharacterStatsList.vue.

---
+++ src/components/users/UserCharacterStatsList.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="stats-list">
+    <li
+      v-for="(stat, key) in stats"
+      :key="key"
+      class="stats-list-item"
+    >
+      <p class="stat">
+        {{ $t(key.toLowerCase()) }}: {{ stat }}
+      </p>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'UserCharacterStatsList',
+  props: {
+    stats: {
+      type: Object,
+      required: true,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+.stat {
+  margin: 5px 0;
+}
+</style>
