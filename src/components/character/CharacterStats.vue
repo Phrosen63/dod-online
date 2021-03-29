8531bcb756a1f8b3f762b715ef31d21f50c82bd7
Phrosen63 <peter.persson1@gmail.com>
2021-03-29 18:23:20 +0200

    Add missing semi-colon.

--- src/components/character/CharacterStats.vue
+++ src/components/character/CharacterStats.vue
@@ -83,7 +83,7 @@ export default {
           const data = {
             hasError: true,
             message: e,
-          }
+          };
           this.$store.commit('setError', data);
         });
       }
